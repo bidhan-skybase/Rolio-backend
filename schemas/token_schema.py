6b962523ff3a80f88643b99dcea998db33a4e02a
bidhan-skybase <bidhan@skybase.com.np>
2025-10-29 08:32:54 +0545

    feat: Add verify access token functionality

--- schemas/token_schema.py
+++ schemas/token_schema.py
@@ -1,10 +1,5 @@
 from pydantic import BaseModel, ConfigDict, EmailStr
 
-
-class TokenCreate(BaseModel):
-    email:EmailStr
-
-
 class TokenResponse(BaseModel):
     model_config=ConfigDict(from_attributes=True)
     token:str
--- utils/token_utils.py
+++ utils/token_utils.py
@@ -10,7 +10,16 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 10080 #7 DAYS
 
 def create_access_token(data: dict| None = None):
     to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + timedelta(minutes=1080)
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+def verify_access_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload 
+    except jwt.ExpiredSignatureError:
+        raise Exception("Token expired")
+    except jwt.InvalidTokenError:
+        raise Exception("Invalid token")
